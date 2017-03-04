ee709d167ff451d266452afdf760fb5ddc5d55b1
Edwin Corea <Edwin Corea>
2017-03-03 20:46:14 -0600

    Components and the shadow DOM.

--- primer/example1.js
+++ primer/example1.js
@@ -1,1 +1,12 @@
-console.log("Hei");
+class AppComponent extends React.Component {
+    render(){
+        return React.createElement(
+            "h1",
+            {className: "header"},
+            "Hello, World");        
+    }
+}
+
+ReactDOM.render(
+    React.createElement(AppComponent, null),    
+    document.getElementById("application"));
