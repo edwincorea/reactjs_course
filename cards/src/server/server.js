4a80a2b46dce5ea2951c6238961937ce9b028989
Edwin Corea <Edwin Corea>
2017-03-05 17:06:11 -0600

    ESLint configuration

--- cards/src/server/server.js
+++ cards/src/server/server.js
@@ -4,3 +4,20 @@ console.log(`Hello ${test}`);
 const obj = {hei: 1};
 const obj2 = {...obj, prop: 2};
 console.log(obj2);
+
+class AppComponent {
+    static PropTypes = {
+        test: "1"
+    }
+}
+
+console.log(<AppComponent />);
+
+const a = 1;
+switch (a){
+    case 1: 
+        console.log("1");
+        break;
+}
+
+
