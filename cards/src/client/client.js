51e51a2aaf9dd8ca30379ee1a43a52f2501bd958
Edwin Corea <Edwin Corea>
2017-03-26 15:45:22 -0600

    Webpack: HMR2 - Scripts.

--- cards/src/client/client.js
+++ cards/src/client/client.js
@@ -1,8 +1,28 @@
 import "./client.scss";
 
-import React from "react";
 import ReactDom from "react-dom";
 
-import AppContainer from "./components/app";
+function main() {
+    //AppContainer is reloaded everytime main is called
+    //const AppContainer = require("./components/app").default;
+    //ReactDom.render(<AppContainer />, document.getElementById("mount"));
 
-ReactDom.render(<AppContainer />, document.getElementById("mount"));
+    const routes = require("./routes").default();
+    ReactDom.render(routes, document.getElementById("mount"));
+}
+
+main();
+
+if (module.hot) {
+    //problem #2: handle hmr of routes instead of single components.
+    //Solution: accept routes instead of single component
+    
+    //triggered whenever the module or one of its dependencies changes
+    // module.hot.accept("./components/app", () => {
+    //     main();
+    // });
+
+    module.hot.accept("./routes", () => {
+        main();
+    });    
+}
--- cards/src/client/components/app.js
+++ cards/src/client/components/app.js
@@ -3,8 +3,16 @@ import "./app.scss";
 import React, {Component} from "react";
 
 class AppContainer extends Component {
+    //problem #1: everytime component gets reloaded, it is remounted.
+    //Solution: 
+    //1. react-hot-loader/babel in .babelrc
+    //2. react-hot-loader/patch in webpack.config.js
+    componentDidMount() {
+        console.log("Component hierarchy is recreated!");
+    }    
+
     render() {
-        return <h1>Hello World</h1>;
+        return <h1>Hello World!</h1>;
     }
 } 
 
