2be2b9e9b4abfa90fa19fafd0407f203fbdd9e13
Edwin Corea <Edwin Corea>
2017-04-15 16:05:30 -0600

    Lobby: Part 1 - Game List

--- cards/src/client/components/lobby.js
+++ cards/src/client/components/lobby.js
@@ -5,21 +5,87 @@ import React, {Component} from "react";
 class LobbyContainer extends Component {
     constructor(props) {
         super(props);
+
+        this._joinGame = this._joinGame.bind(this);
+    }
+
+    _joinGame(game) {
+        console.log(`TODO: Join Game ${game.title}`);        
     }
 
     render() {
-        return <p>Lobby!</p>;
+        //in the future get games from a different source such as an API
+        const games = [
+            {title: "Game 1", id: 1, players: ["player1", "player2", "player3"]},
+            {title: "Game 2", id: 2, players: ["player4", "player5", "player6"]},
+            {title: "Game 3", id: 3, players: ["player7", "player8", "player9"]},
+            {title: "Game 4", id: 4, players: ["player10", "player11", "player12"]},
+        ];
+        
+        return (
+            <div className="c-lobby">
+                <GameList games={games} joinGame={this._joinGame} />
+            </div>
+        );
     }
 }
 
 class LobbySidebar extends Component {
     constructor(props) {
         super(props);
+
+        this._login = this._login.bind(this);
+        this._createGame = this._createGame.bind(this);
+    }
+
+    _login() {
+        console.log("TODO: Implement Login");
+    }
+
+    _createGame() {
+        console.log("TODO: Create Game");
     }
 
     render() {
-        return <p>Lobby Sidebar!</p>;
+        const canLogin = true;
+        const canCreateGame = true;
+        const createGameInProgress = false;
+
+        return (
+            <section className="c-lobby-sidebar">
+                <div className="m-sidebar-buttons">
+                    {!canLogin ? null : 
+                        <button className="m-button primary" onClick={this._login}>Login</button>}
+                    {!canCreateGame ? null : 
+                        <button 
+                            className="m-button good"
+                            onClick={this._createGame}
+                            disabled={createGameInProgress}>
+                            Create Game
+                        </button>}                        
+                </div>
+            </section>
+        );
     }    
+}
+
+//stateless component
+function GameList({games, joinGame}) {
+    return (
+        <section className="c-game-list">
+            {games.length > 0 ? null : 
+                <div className="no-games">There are no games yet :(</div>}
+            
+            {games.map(g => 
+                <div className="game" key={g.id} onClick={() => joinGame(g)}>
+                    <div className="title">{g.title}</div>
+                    <div className="players">
+                        {g.players.join(", ")}
+                    </div>
+                    <div className="join-game">Join Game</div>
+                </div>)}
+        </section>
+    );
 }
 
 export default {
