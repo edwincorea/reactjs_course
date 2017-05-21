0669398e6c9a2f4d8abd4b811a98aea0960fbf76
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-05-20 19:14:56 -0600

    Print Action Utility.

--- cards/src/client/client.js
+++ cards/src/client/client.js
@@ -1,5 +1,6 @@
 import "./client.scss";
 
+import _ from "lodash";
 import React from "react";
 import ReactDom from "react-dom";
 
@@ -15,13 +16,17 @@ import createStores from "./stores";
 const dispatcher = new Dispatcher();
 const services = {dispatcher};
 
+if (IS_DEVELOPMENT) {
+    dispatcher.on("*", printAction);
+}
+
 //------------------------------------------------------
 // Stores
 const stores = createStores(services);
 
 //------------------------------------------------------
 // Render
-function main() {
+const main = () => {
     const routes = require("./routes").default();
     ReactDom.render(
         <StoreProvider stores={stores} services={services}>
@@ -30,7 +35,7 @@ function main() {
             </Router>
         </StoreProvider>, 
         document.getElementById("mount"));
-}
+};
 
 //------------------------------------------------------
 // Misc
@@ -44,3 +49,23 @@ if (module.hot) {
 // Go!
 main();
 
+//------------------------------------------------------
+// Helpers
+function printAction(action) {
+    if (action.hasOwnProperty("status")) {
+        let style = null;
+        switch(action.status) {
+            case A.STATUS_REQUEST: style = "color: blue"; break;
+            case A.STATUS_FAIL: style = "color: red"; break;
+            case A.STATUS_SUCCESS: style = "color: green"; break;        
+        }
+
+        console.log(`%c${action.type}`, `${style}; font-weight: bold; background: #eee; width: 100%; display: block;`);
+    } else {
+        console.log(`%c${action.type}`, `background: #ddd;`);
+    }
+
+    const result = _.omit(action, ["type", "status"]);
+    if (_.keys(result).length)
+        console.log(result);
+}
