08d61faa23d8218258cf8f2392b2d768b1a278ae
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-07-23 11:54:32 -0600

    Diff: Part 2 - Arrays

--- cards/src/server/shared/__tests/diff.js
+++ cards/src/server/shared/__tests/diff.js
@@ -2,6 +2,7 @@ import _ from "lodash";
 
 import {makeDiff, mergeDiff, IS_UNCHANGED} from "../diff";
 
+//Tests harness
 runDiffTests({
     scalar: {
         "changed on numbers": [
@@ -20,6 +21,88 @@ runDiffTests({
             null, null,
             IS_UNCHANGED
         ]
+    },
+    array: {
+        "throws on invalid type": () => {
+            expect(() => makeDiff("test", [])).toThrow();
+        },
+        "unchanged on empty": () => [
+            [], [], 
+            IS_UNCHANGED
+        ],
+        "empties array": [
+            [1, 2, 3], [],
+            {$splice: [0, 3]}
+        ]        
+    },
+    simpleArrays: {
+        "unchanged": [
+            [1, 2, 3], [1, 2, 3],
+            IS_UNCHANGED
+        ],
+        "splice": [
+            [1, 2, 3], [1, 2, 4, 5],
+            {$splice: [2, 1, 4, 5]}
+        ],
+        "add one item at end": [
+            [1, 2, 3], [1, 2, 3, 4],
+            {$splice: [3, 0, 4]}
+        ],
+        "remove one item at end": [
+            [1, 2, 3], [1, 2],
+            {$splice: [2, 1]}
+        ],
+        "remove one in the middle": [
+            [1, 2, 3], [1, 3],
+            {$splice: [1, 2, 3]}
+        ]
+    },
+    complexArrays: {
+        "unchanged": [
+            [
+                {id: 1, text: "text1"},
+                {id: 2, text: "text2"},
+                {id: 3, text: "text3"}
+            ],
+            [
+                {id: 1, text: "text1"},
+                {id: 2, text: "text2"},
+                {id: 3, text: "text3"}
+            ],
+            IS_UNCHANGED            
+        ],
+        "removes one": [
+            [
+                {id: 1, text: "text1"},
+                {id: 2, text: "text2"}
+            ],
+            [
+                {id: 1, text: "text1"}
+            ],
+            {$update: {}, ids: [1]}
+        ],
+        "reorder": [
+            [
+                {id: 1, text: "text1"},
+                {id: 2, text: "text2"}
+            ],
+            [
+                {id: 2, text: "text2"},
+                {id: 1, text: "text1"}
+            ],
+            {$update: {}, ids: [2, 1]}
+        ],
+        "update": [
+            [
+                {id: 1, text: "text1"},
+                {id: 2, text: "text2"}
+            ],
+            [
+                {id: 1, text: "text1"},
+                {id: 2, text: "text2Updated"}
+            ],
+            {$update: {2: {text: "text2Updated"}}, ids: [1, 2]}
+        ]        
     }
 });
 
